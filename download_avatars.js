447608e44d52da51c955bff466e22bf463cfab6a
Jessica Winters <jskawinters@gmail.com>
2018-10-30 03:18:37 +0000

    Added error handler if user does not add 2 args to prevent the program from attempting a request

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${secrets.GITHUB_TOKEN}`
@@ -20,17 +20,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors(args[0], args[1], function(err, result) {
-  const contributors = JSON.parse(result);
+  if (args.length !== 2) {
+    console.log('Please specify <repoOwner> and <repoName> to download GitHub avatars. e.g. jquery jquery');
+  } else {
+    const contributors = JSON.parse(result);
 
-  contributors.forEach((contributor) => {
-    downloadImageByURL(contributor.avatar_url, 'avatars/' + contributor.login + '.jpg');
-  });
+    contributors.forEach((contributor) => {
+      downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}.jpg`);
+    });
+
+    console.log('Finished downloading avatars from specified GitHub repo!');
+  }
 });
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-    .on('error', function (err) {
-      console.log('Errors:', err); 
-    })
-    .pipe(fs.createWriteStream(filePath));
+  .on('error', function (err) {
+    console.log('Errors:', err); 
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
