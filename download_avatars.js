de0a1a60430625a03b9d9307b488e7e45d671f43
Jessica Winters <jskawinters@gmail.com>
2018-10-30 02:09:00 +0000

    Setup downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -22,12 +22,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
-  console.log('Errors:', err);
-  console.log('Result:', result);
-
   const contributors = JSON.parse(result);
 
   contributors.forEach((contributor) => {
     console.log(contributor.avatar_url);
   });
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      console.log('Errors:', err); 
+    })
+    .on('response', function (response) {
+      // console.log('Result:', response);
+      console.log(url, filePath);
+    });
+    // .pipe(fs.createWriteStream('./downloaded.html'));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
